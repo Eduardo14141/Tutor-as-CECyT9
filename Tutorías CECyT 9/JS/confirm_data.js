a2199e1fc0d7b447a243e93d6062ebfc1bb752ba
Eduardo14141 <edu_0601@live.com.mx>
2019-04-23 00:37:01 -0500

    more changes

--- Tutorías CECyT 9/JS/confirm_data.js
+++
@@ -1,9 +0,0 @@
-window.onload = ()=>{
-    initPicker();
-};
-
-const initPicker = ()=>{
-    document.querySelector("#photo").addEventListener("click", ()=>{
-        document.querySelector("#file-input").click();
-    });
-};
