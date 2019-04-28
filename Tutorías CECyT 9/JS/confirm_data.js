5538a6fe757e5952195b70b9fe034a9d06f7e76a
Eduardo14141 <edu_0601@live.com.mx>
2019-04-28 00:33:11 -0500

    validations

---
+++ Tutorías CECyT 9/JS/confirm_data.js
@@ -0,0 +1,26 @@
+window.onload = () => {
+    initPicker();
+    initFormValidation();
+};
+
+const initPicker = () => {
+    document.querySelector("#photo").addEventListener("click", () => {
+        document.querySelector("#file-input").click();
+    });
+};
+const initFormValidation = ()=>{
+    document.querySelector("#confirm-btn").addEventListener("click", ()=>{
+        let everything_ok = true;
+        /*if(!validateName())
+            return false;
+        if(!lastnameValidators())
+            return false;
+        if(!validateBoleta())
+            return false;
+        if(!validateSex())
+            return false;*/
+        if(!validateEmail())
+            return false;
+        
+    });
+};
